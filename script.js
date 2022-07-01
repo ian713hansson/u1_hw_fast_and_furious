741b3442bbac0dd5f5d1b7f09f23a5a0580d1936
Ian Hansson <ian713hansson@gmail.com>
2022-07-01 06:46:17 -0500

    F&F hw finished

--- script.js
+++ script.js
@@ -20,16 +20,40 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
+  function changeTitle() {
+    newTitle = "Dom Toretto, danger junkie";
+    document.querySelector('#main-title').textContent = newTitle
+  }
+  changeTitle()
 
   // Part 2
+document.querySelector('body').style.backgroundColor = 'blue';
 
   // Part 3
+  function removeLast() {
+    var list=document.getElementById('favorite-things');
+    var listItems = list.getElementsByTagName('li');
+    var last=listItems [listItems.length-1];
+    list.removeChild(last);
+  }
+  removeLast()
 
   // Part 4
+document.getElementsByClassName('special-title')[0].style.fontSize = '2em';
 
   // Part 5
-
+  let races = document.querySelector('#past-races')
+  races.removeChild(races.children[3])
+  
   // Part 6
-
+  var node = document.createElement('li');
+  node.appendChild(document.createTextNode('High Bridge'));
+   
+  document.querySelector('#past-races').appendChild(node);  
   // Part 7
+  const blogPost = document.createElement('div')
+  let newPost = document.querySelector('.main')
+  blogPost.className = 'blog-post purple'
+  blogPost.innerHTML = '<h1>High Bridge</h1> <p>I jumped over that dang ole bridge</p>'
+  newPost.appendChild(blogPost)
 }
